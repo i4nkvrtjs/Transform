eeee495566ed04efa0d6480f794b3bd6f012ef54
i4nkvrtjs <valentin.pineyro@gmail.com>
2026-06-08 20:29:08 -0300

    V1.0

--- Scenes/game_over.gd
+++ Scenes/game_over.gd
@@ -1,17 +1,92 @@
 extends Control
 
-func _ready() -> void:
-	$ScoreLabel.text = "SCORE: " + str(GameData.final_score)
-	
-	$RetryButton.pressed.connect(_on_retry_pressed)
-	$QuitButton.pressed.connect(_on_quit_pressed)
-	$AudioStreamPlayer.play()
+@onready var retry_button = $RetryButton
+@onready var quit_button = $QuitButton
+
+var retry_normal
+var retry_hover
+
+var quit_normal
+var quit_hover
+
+func _ready():
+
+	retry_normal = retry_button.texture_normal
+	retry_hover = retry_button.texture_hover
+
+	quit_normal = quit_button.texture_normal
+	quit_hover = quit_button.texture_hover
+
+	retry_button.grab_focus()
+
+	retry_button.pressed.connect(
+		_on_retry_pressed
+	)
+
+	quit_button.pressed.connect(
+		_on_quit_pressed
+	)
+
+	update_button_visuals()
+
+func _process(_delta):
+
+	if get_viewport().gui_get_focus_owner() == null:
+
+		retry_button.grab_focus()
+
+	update_button_visuals()
+
+func _unhandled_input(event):
+
+	if event.is_action_pressed("ability"):
+
+		var focused = (
+			get_viewport()
+			.gui_get_focus_owner()
+		)
+
+		if focused is BaseButton:
+
+			focused.pressed.emit()
+
+func update_button_visuals():
+
+	var focused = (
+		get_viewport()
+		.gui_get_focus_owner()
+	)
+
+	if focused == retry_button:
+
+		retry_button.texture_normal = retry_hover
+
+	else:
+
+		retry_button.texture_normal = retry_normal
+
+	if focused == quit_button:
+
+		quit_button.texture_normal = quit_hover
+
+	else:
+
+		quit_button.texture_normal = quit_normal
 
 func _on_retry_pressed():
+
 	$AudioButton.play()
-	get_tree().change_scene_to_file("res://Scenes/World/main.tscn")
+
+	await $AudioButton.finished
+
+	get_tree().change_scene_to_file(
+		"res://Scenes/World/main.tscn"
+	)
 
 func _on_quit_pressed():
+
 	$AudioButton.play()
+
 	await $AudioButton.finished
+
 	get_tree().quit()
--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,20 +1,91 @@
 extends Control
 
+@onready var play_button = $PlayButton
+@onready var quit_button = $QuitButton
+
+var play_normal
+var play_hover
+
+var quit_normal
+var quit_hover
+
 func _ready() -> void:
-	
-	$PlayButton.pressed.connect(
+
+	play_normal = play_button.texture_normal
+	play_hover = play_button.texture_hover
+
+	quit_normal = quit_button.texture_normal
+	quit_hover = quit_button.texture_hover
+
+	play_button.grab_focus()
+
+	play_button.pressed.connect(
 		_on_play_pressed
 	)
-	$QuitButton.pressed.connect(
+
+	quit_button.pressed.connect(
 		_on_quit_pressed
 	)
+
 	$AudioStreamPlayer.play()
 
+	update_button_visuals()
+
+func _process(_delta):
+
+	if get_viewport().gui_get_focus_owner() == null:
+
+		play_button.grab_focus()
+
+	update_button_visuals()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ability"):
+
+		var focused = (
+			get_viewport()
+			.gui_get_focus_owner()
+		)
+
+		if focused is BaseButton:
+
+			focused.pressed.emit()
+
+func update_button_visuals():
+
+	var focused = (
+		get_viewport()
+		.gui_get_focus_owner()
+	)
+
+	if focused == play_button:
+
+		play_button.texture_normal = play_hover
+
+	else:
+
+		play_button.texture_normal = play_normal
+
+	if focused == quit_button:
+
+		quit_button.texture_normal = quit_hover
+
+	else:
+
+		quit_button.texture_normal = quit_normal
+
 func _on_play_pressed():
+
 	$AudioButton.play()
-	get_tree().change_scene_to_file("res://Scenes/World/main.tscn")
+
+	get_tree().change_scene_to_file(
+		"res://Scenes/World/main.tscn"
+	)
 
 func _on_quit_pressed():
+
 	$AudioButton.play()
+
 	await $AudioButton.finished
+
 	get_tree().quit()
